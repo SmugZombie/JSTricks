12f523ce837ff94f6499f7bd84c1a828b7472a4b
Ron Egli <ron@r-egli.com>
2015-12-03 09:47:09 -0700

    Create nukeCookies.js

---
+++ cookieNuker/nukeCookies.js
@@ -0,0 +1,12 @@
+// Calling this function will delete all cookies, reguardless of name, for a domain.
+
+function nukeCookies() {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+    	var cookie = cookies[i];
+    	var eqPos = cookie.indexOf("=");
+    	var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    	document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+}
