33506a9d6127deae8369e046b3729528ff69898f
Ron Egli <ron@r-egli.com>
2015-12-03 09:48:58 -0700

    Create htmlEntities.js

---
+++ htmlEntities/htmlEntities.js
@@ -0,0 +1,5 @@
+// Encode HTML Entities
+function htmlEntities(str){ return String(str).replace(/&/g, '&amp;').replace(/"/g, '&quot;').replace(/'/g, '&#39;').replace(/</g, '&lt;').replace(/>/g, '&gt;'); }
+
+// Decode HTML Entities
+function htmlEntitiesDecode(str){ return String(str).replace(/&quot;/g, '"').replace(/&#39;/g, "'").replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&amp;/g, '&'); }
